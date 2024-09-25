4fe532c184384fad6a822c45f6027dddbc33a565
zainmasood22ltsb <154008201+zainmasood22ltsb@users.noreply.github.com>
2024-09-26 02:08:02 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,43 @@
+for(var i = 9; i>=1; i = i-2){
+
+    for(var  k = 9; k>=i; k--){
+     document.write("&nbsp")
+}
+    for(var j = 1; j<=i; j++){
+        document.write("*")
+    }
+   document.write("<br>")  
+}
+for(var  i = 9; i>=1; i = i-2){
+
+    for(var k = 9; k>=i; k--){
+     document.write("&nbsp")
+}
+    for(var  j = 1; j<=i; j++){
+        document.write("*")
+    }
+   document.write("<br>")  
+}
+
+
+
+for(var  i = 1; i<10; i = i+2){
+    
+     for(var k = 9; k>=i; k--){
+         document.write("&nbsp;") 
+     }
+
+    for(var  j = 1; j<=i; j++){
+        document.write("*") 
+       
+    }
+    document.write("<br>") 
+ }
+
+
+
+
+
+
+
+
